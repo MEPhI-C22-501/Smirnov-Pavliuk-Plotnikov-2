e1d2451368e69814a26d364def638a910ef552aa
Ilya Smirnov <120823664+pachecou@users.noreply.github.com>
2024-12-09 15:15:54 +0300

    Update writeBack.vhd

--- writeBack.vhd
+++ writeBack.vhd
@@ -1,35 +1,39 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL; 
+
 
 entity WriteBack is
     port (
         i_clk          : in  STD_LOGIC;
         i_rst          : in  STD_LOGIC;
-        i_read_data    : in  STD_LOGIC_VECTOR(31 downto 0); --данные из регистров
-        i_ALUResult    : in  STD_LOGIC_VECTOR(31 downto 0); --данные из АЛУ
-        i_resultSrc    : in  STD_LOGIC;  -- выбор места, откуда брать результат ("0" - регистры, "1" - АЛУ)
-        i_regWrite     : in  STD_LOGIC;  -- разрешение на запись
-        
-        o_result       : out STD_LOGIC_VECTOR(31 downto 0) -- результат, который принимает LSU
+        i_read_data    : in  STD_LOGIC_VECTOR(31 downto 0); 
+        i_ALUResult    : in  STD_LOGIC_VECTOR(31 downto 0); 
+        i_resultSrc    : in  STD_LOGIC;                
+        i_regWrite     : in  STD_LOGIC;               
+        o_result       : out STD_LOGIC_VECTOR(31 downto 0);   
+        o_regWrite     : out STD_LOGIC
     );
 end entity WriteBack;
 
 architecture Behavioral of WriteBack is
 begin
-    process(i_clk)
-    begin
-        if rising_edge(i_clk) then
-            if i_rst = '1' then
-                o_result <= (others => '0');
-            elsif i_regWrite = '1' then
-                if i_resultSrc = '1' then
-                    o_result <= i_read_data;
-                else
-                    o_result <= i_ALUResult;
-                end if;
-            end if;
+   process(i_rst, i_clk)
+   begin
+    if i_rst = '1' then  
+        o_result <= (others => '0');
+        o_regWrite <= '0';
+    elsif rising_edge(i_clk) then 
+		  
+		  if i_resultSrc = '1' then
+           o_result <= i_ALUResult;
+        else
+           o_result <= i_read_data;
         end if;
-    end process;
+		  
+		  o_regWrite <= i_regWrite;
+    end if;
+  end process;
 end Behavioral;
+
