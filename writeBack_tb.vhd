8798abc9eacf48cd9f29ddcaf19f16042f05863b
Gleb <72437559+glebpav@users.noreply.github.com>
2025-01-23 20:51:27 +0300

    Update writeBack_tb.vhd

--- writeBack_tb.vhd
+++ writeBack_tb.vhd
@@ -7,13 +7,14 @@ end entity;
 
 architecture tb of WriteBack_tb is
 
-		 signal i_clk          :   STD_LOGIC := '0';
+	   signal i_clk          :   STD_LOGIC := '0';
        signal i_rst          :   STD_LOGIC := '1';
        signal i_read_data    :   STD_LOGIC_VECTOR(31 downto 0); --данные из регистров
        signal i_ALUResult    :   STD_LOGIC_VECTOR(31 downto 0); --данные из АЛУ
        signal i_resultSrc    :   STD_LOGIC;  -- выбор места, откуда брать результат ("0" - регистры, "1" - АЛУ)
        signal i_regWrite     :   STD_LOGIC;  -- разрешение на запись
        signal o_result       :   STD_LOGIC_VECTOR(31 downto 0);
+	   signal o_regWrite     :   STD_LOGIC;
 
   component WriteBack 
 	port (
@@ -24,7 +25,8 @@ architecture tb of WriteBack_tb is
         i_resultSrc    : in  STD_LOGIC;  -- выбор места, откуда брать результат ("0" - регистры, "1" - АЛУ)
         i_regWrite     : in  STD_LOGIC;  -- разрешение на запись
         
-        o_result       : out STD_LOGIC_VECTOR(31 downto 0) -- результат, который принимает LSU
+        o_result       : out STD_LOGIC_VECTOR(31 downto 0); -- результат, который принимает LSU
+		o_regWrite	   : out STD_LOGIC
     );
   end component;  
   
@@ -35,8 +37,9 @@ architecture tb of WriteBack_tb is
            i_read_data    : out  STD_LOGIC_VECTOR(31 downto 0); 
            i_ALUResult    : out  STD_LOGIC_VECTOR(31 downto 0); 
            i_resultSrc    : out  STD_LOGIC;  
-           i_regWrite     : out  STD_LOGIC;  
-           o_result       : in STD_LOGIC_VECTOR(31 downto 0));
+           i_regWrite     : out  STD_LOGIC 
+		);
+           --o_result       : in STD_LOGIC_VECTOR(31 downto 0));
    end component; 
 
 begin
@@ -48,7 +51,8 @@ begin
     i_ALUResult => i_ALUResult,
     i_resultSrc => i_resultSrc,
     i_regWrite => i_regWrite,
-    o_result => o_result
+    o_result => o_result,
+	o_regWrite => o_regWrite
   );
 
   tester : WriteBack_tester 
@@ -58,8 +62,8 @@ begin
     i_read_data => i_read_data,
     i_ALUResult => i_ALUResult,
     i_resultSrc => i_resultSrc,
-    i_regWrite => i_regWrite,
-    o_result => o_result
+    i_regWrite => i_regWrite
+    --o_result => o_result
   );
 
 end architecture tb;
