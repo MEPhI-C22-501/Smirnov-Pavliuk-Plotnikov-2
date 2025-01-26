7fa97f2dda9cb38c00f64d6fba7e0af1e6198bf1
Gleb <72437559+glebpav@users.noreply.github.com>
2025-01-26 15:10:50 +0300

    Update writeBack_tester.vhd

--- writeBack_tester.vhd
+++ writeBack_tester.vhd
@@ -1,59 +1,98 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity WriteBack_tester is
-    Port ( i_clk          : out  STD_LOGIC;
+    Port ( 
+		   i_clk          : out  STD_LOGIC;
            i_rst          : out  STD_LOGIC;
-           i_read_data    : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
-           i_ALUResult    : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
-           i_resultSrc    : out  STD_LOGIC;  
-           i_regWrite     : out  STD_LOGIC  
-           --o_result       : in STD_LOGIC_VECTOR(31 downto 0)
-			  );
+		   
+           i_datamem_result     : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
+           i_ALU_result         : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
+		   i_CSR_result 		: out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+           
+		   i_result_src    		: out  STD_LOGIC_VECTOR(1 downto 0)  := "00"
+	);
 end entity WriteBack_tester;
 
 architecture tester of WriteBack_tester is
-  signal clk : std_logic := '0';
-  signal rst : std_logic := '0';
-  constant clk_period : time := 10 ns;
-begin
-	clk_process : process 
-   begin
-        i_clk <= '1';
-        wait for clk_period / 2;
-        i_clk <= '0';
-        wait for clk_period / 2;
-   end process;
-	test_process : process
-   begin
-			
-			i_rst <='0';
-            i_regWrite <= '0'; 
-			i_resultSrc <= '0'; 
-			wait for clk_period * 2;
-			i_regWrite <= '1';	
-			wait for clk_period;
 	
-			i_ALUResult <= x"0000000A"; 
-			i_read_data <= x"FFFFFFFF";
-			wait for clk_period * 2;
-			i_resultSrc <= '1'; 
-			wait for clk_period * 2;
-							 
-			i_resultSrc <= '0';
-			wait for clk_period * 2; 
-			i_ALUResult <= x"12345678"; 
-			i_read_data <= x"87654321";
-			wait for clk_period * 2;
-					 
-			i_resultSrc <= '1';
-			wait for clk_period * 2;  
-			i_rst <= '1';
-		    wait for clk_period * 2;  
-			wait for clk_period * 2; 
-			i_regWrite <= '1';
+	signal clk : std_logic := '0';
+	constant clk_period : time := 10 ns;
+  
+	procedure wait_clk(constant j: in integer) is 
+        variable ii: integer := 0;
+        begin
+        while ii < j loop
+            if (rising_edge(clk)) then
+                ii := ii + 1;
+            end if;
+            wait for 10 ps;
+        end loop;
+    end;
+  
+	begin
 
-  end process;
+	clk <= not clk after clk_period / 2;
+    i_clk <= clk;
+
+process
+begin
+	
+	i_rst <= '1'; -- Удерживаем сброс
+	i_ALU_result <= (others => '0');
+	i_datamem_result <= (others => '0');
+	i_CSR_result <= (others => '0');
+	--i_result_src <= "00";
+	wait_clk(2); 
+
+	
+	i_rst <= '0';
+	wait_clk(1);
+
+	i_ALU_result <= x"0000000A"; 
+	i_datamem_result <= x"FFFFFFFF";
+	i_CSR_result <= x"55555555";
+
+	-- Выбор результата из памяти данных
+	i_result_src <= "01"; 
+	wait_clk(2); 
+
+	-- Выбор результата из АЛУ
+	i_result_src <= "00";
+	wait_clk(2);
+	
+	-- Выбор результата из CSR
+	i_result_src <= "10";
+	wait_clk(2);
+	
+	-- Выбор памяти данных
+	i_result_src <= "01"; 
+	wait_clk(2);
+	i_datamem_result <= x"87654321";
+	wait_clk(2);
+	
+	-- Выбор результата из АЛУ
+	i_result_src <= "00"; 
+	wait_clk(2);
+	i_ALU_result <= x"12345678"; 
+	wait_clk(2);
+	
+	-- Выбор результата из CSR
+	i_result_src <= "10";
+	wait_clk(2);
+	i_CSR_result <= x"10101010";
+	wait_clk(2);
+
+	-- Сброс
+	i_rst <= '1';
+	wait_clk(2);
+	
+	i_result_src <= "11";
+
+	-- Завершение симуляции
+	wait;
+end process;
+
 
 end architecture tester;
