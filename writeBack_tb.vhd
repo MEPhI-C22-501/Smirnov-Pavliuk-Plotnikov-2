c5bdd7429be576accfd0c96bd6a5fc75eb7a382e
Plotnikov Artem <115216997+pltnkvart@users.noreply.github.com>
2025-01-26 14:43:01 +0300

    new test bench

--- writeBack_tb.vhd
+++ writeBack_tb.vhd
@@ -7,63 +7,57 @@ end entity;
 
 architecture tb of WriteBack_tb is
 
-	   signal i_clk          :   STD_LOGIC := '0';
-       signal i_rst          :   STD_LOGIC := '1';
-       signal i_read_data    :   STD_LOGIC_VECTOR(31 downto 0); --данные из регистров
-       signal i_ALUResult    :   STD_LOGIC_VECTOR(31 downto 0); --данные из АЛУ
-       signal i_resultSrc    :   STD_LOGIC;  -- выбор места, откуда брать результат ("0" - регистры, "1" - АЛУ)
-       signal i_regWrite     :   STD_LOGIC;  -- разрешение на запись
-       signal o_result       :   STD_LOGIC_VECTOR(31 downto 0);
-	   signal o_regWrite     :   STD_LOGIC;
+    signal clk            : STD_LOGIC := '0';
+    signal rst            : STD_LOGIC := '1';
+    signal datamem_result : STD_LOGIC_VECTOR(31 downto 0); -- данные из памяти данных
+    signal ALU_result     : STD_LOGIC_VECTOR(31 downto 0); -- данные из АЛУ
+    signal CSR_result     : STD_LOGIC_VECTOR(31 downto 0); -- данные из CSR
+    signal result_src     : STD_LOGIC_VECTOR(1 downto 0);  -- выбор источника результата
+    signal result         : STD_LOGIC_VECTOR(31 downto 0); -- результат, который принимает LSU
 
-  component WriteBack 
-	port (
-        i_clk          : in  STD_LOGIC;
-        i_rst          : in  STD_LOGIC;
-        i_read_data    : in  STD_LOGIC_VECTOR(31 downto 0); --данные из регистров
-        i_ALUResult    : in  STD_LOGIC_VECTOR(31 downto 0); --данные из АЛУ
-        i_resultSrc    : in  STD_LOGIC;  -- выбор места, откуда брать результат ("0" - регистры, "1" - АЛУ)
-        i_regWrite     : in  STD_LOGIC;  -- разрешение на запись
-        
-        o_result       : out STD_LOGIC_VECTOR(31 downto 0); -- результат, который принимает LSU
-		o_regWrite	   : out STD_LOGIC
-    );
-  end component;  
-  
-  component WriteBack_tester  
-   port (  
-           i_clk          : out  STD_LOGIC;
-           i_rst          : out  STD_LOGIC;
-           i_read_data    : out  STD_LOGIC_VECTOR(31 downto 0); 
-           i_ALUResult    : out  STD_LOGIC_VECTOR(31 downto 0); 
-           i_resultSrc    : out  STD_LOGIC;  
-           i_regWrite     : out  STD_LOGIC 
-		);
-           --o_result       : in STD_LOGIC_VECTOR(31 downto 0));
-   end component; 
+    component WriteBack 
+        port (
+            i_clk            : in  STD_LOGIC;
+            i_rst            : in  STD_LOGIC;
+            i_ALU_result     : in  STD_LOGIC_VECTOR(31 downto 0); 
+            i_datamem_result : in  STD_LOGIC_VECTOR(31 downto 0); 
+            i_CSR_result     : in  STD_LOGIC_VECTOR(31 downto 0);
+            i_result_src     : in  STD_LOGIC_VECTOR(1 downto 0);  -- "00" - ALU; "01" - datamem; "10" - CSR
+            o_result         : out STD_LOGIC_VECTOR(31 downto 0)  
+        );
+    end component;  
+
+    component WriteBack_tester  
+        port (  
+            i_clk            : out STD_LOGIC;
+            i_rst            : out STD_LOGIC;
+            i_datamem_result : out STD_LOGIC_VECTOR(31 downto 0); 
+            i_ALU_result     : out STD_LOGIC_VECTOR(31 downto 0); 
+            i_CSR_result     : out STD_LOGIC_VECTOR(31 downto 0);
+            i_result_src     : out STD_LOGIC_VECTOR(1 downto 0)  
+        );
+    end component; 
 
 begin
-  uut : WriteBack 
-  port map (
-    i_clk => i_clk,
-    i_rst => i_rst,
-    i_read_data => i_read_data,
-    i_ALUResult => i_ALUResult,
-    i_resultSrc => i_resultSrc,
-    i_regWrite => i_regWrite,
-    o_result => o_result,
-	o_regWrite => o_regWrite
-  );
+    uut : WriteBack 
+    port map (
+        i_clk            => clk,
+        i_rst            => rst,
+        i_ALU_result     => ALU_result,
+        i_datamem_result => datamem_result,
+        i_CSR_result     => CSR_result,
+        i_result_src     => result_src,
+        o_result         => result
+    );
 
-  tester : WriteBack_tester 
-  port map (
-    i_clk => i_clk,
-    i_rst => i_rst,
-    i_read_data => i_read_data,
-    i_ALUResult => i_ALUResult,
-    i_resultSrc => i_resultSrc,
-    i_regWrite => i_regWrite
-    --o_result => o_result
-  );
+    tester : WriteBack_tester 
+    port map (
+        i_clk            => clk,
+        i_rst            => rst,
+        i_datamem_result => datamem_result,
+        i_ALU_result     => ALU_result,
+        i_CSR_result     => CSR_result,
+        i_result_src     => result_src
+    );
 
 end architecture tb;
