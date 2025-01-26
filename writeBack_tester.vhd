dca18ab60d131bbd31fbc5448762088a503239b3
Plotnikov Artem <115216997+pltnkvart@users.noreply.github.com>
2025-01-26 15:14:39 +0300

    Update writeBack_tester.vhd

--- writeBack_tester.vhd
+++ writeBack_tester.vhd
@@ -4,23 +4,23 @@ use IEEE.numeric_std.all;
 
 entity WriteBack_tester is
     Port ( 
-		   i_clk          : out  STD_LOGIC;
-           i_rst          : out  STD_LOGIC;
-		   
-           i_datamem_result     : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
-           i_ALU_result         : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
-		   i_CSR_result 		: out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+	i_clk          : out  STD_LOGIC;
+	i_rst          : out  STD_LOGIC;
+       
+	i_datamem_result     : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
+        i_ALU_result         : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000"; 
+        i_CSR_result     : out  STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
            
-		   i_result_src    		: out  STD_LOGIC_VECTOR(1 downto 0)  := "00"
-	);
+        i_result_src        : out  STD_LOGIC_VECTOR(1 downto 0)  := "00"
+  );
 end entity WriteBack_tester;
 
 architecture tester of WriteBack_tester is
-	
-	signal clk : std_logic := '0';
-	constant clk_period : time := 10 ns;
   
-	procedure wait_clk(constant j: in integer) is 
+  signal clk : std_logic := '0';
+  constant clk_period : time := 10 ns;
+  
+  procedure wait_clk(constant j: in integer) is 
         variable ii: integer := 0;
         begin
         while ii < j loop
@@ -31,67 +31,72 @@ architecture tester of WriteBack_tester is
         end loop;
     end;
   
-	begin
+  begin
 
-	clk <= not clk after clk_period / 2;
+  clk <= not clk after clk_period / 2;
     i_clk <= clk;
 
 process
 begin
-	
-	i_rst <= '1'; -- Удерживаем сброс
-	i_ALU_result <= (others => '0');
-	i_datamem_result <= (others => '0');
-	i_CSR_result <= (others => '0');
-	--i_result_src <= "00";
-	wait_clk(2); 
+  
+  i_rst <= '1'; -- Удерживаем сброс
+  i_ALU_result <= (others => '0');
+  i_datamem_result <= (others => '0');
+  i_CSR_result <= (others => '0');
+  --i_result_src <= "00";
+  wait_clk(2); 
 
-	
-	i_rst <= '0';
-	wait_clk(1);
+  
+  i_rst <= '0';
+  wait_clk(1);
 
-	i_ALU_result <= x"0000000A"; 
-	i_datamem_result <= x"FFFFFFFF";
-	i_CSR_result <= x"55555555";
+  i_ALU_result <= x"0000000A"; 
+  i_datamem_result <= x"FFFFFFFF";
+  i_CSR_result <= x"55555555";
 
-	-- Выбор результата из памяти данных
-	i_result_src <= "01"; 
-	wait_clk(2); 
+  -- Выбор результата из памяти данных
+  i_result_src <= "01"; 
+  wait_clk(2); 
 
-	-- Выбор результата из АЛУ
-	i_result_src <= "00";
-	wait_clk(2);
-	
-	-- Выбор результата из CSR
-	i_result_src <= "10";
-	wait_clk(2);
-	
-	-- Выбор памяти данных
-	i_result_src <= "01"; 
-	wait_clk(2);
-	i_datamem_result <= x"87654321";
-	wait_clk(2);
-	
-	-- Выбор результата из АЛУ
-	i_result_src <= "00"; 
-	wait_clk(2);
-	i_ALU_result <= x"12345678"; 
-	wait_clk(2);
-	
-	-- Выбор результата из CSR
-	i_result_src <= "10";
-	wait_clk(2);
-	i_CSR_result <= x"10101010";
-	wait_clk(2);
+  -- Выбор результата из АЛУ
+  i_result_src <= "00";
+  wait_clk(2);
+  
+  -- Выбор результата из CSR
+  i_result_src <= "10";
+  wait_clk(2);
+  
+  -- Выбор запрещённого значения
+  i_result_src <= "11";
+  wait_clk(2);
+  
+  -- Выбор памяти данных
+  i_result_src <= "01"; 
+  wait_clk(2);
+  i_datamem_result <= x"87654321";
+  wait_clk(2);
+  
+  -- Выбор результата из АЛУ
+  i_result_src <= "00"; 
+  wait_clk(2);
+  i_ALU_result <= x"12345678"; 
+  wait_clk(2);
+  
+  -- Выбор результата из CSR
+  i_result_src <= "10";
+  wait_clk(2);
+  i_CSR_result <= x"10101010";
+  wait_clk(2);
+  
+  wait_clk(2);
+  -- Сброс
+  i_rst <= '1';
+  wait_clk(2);
+  
+  i_result_src <= "11";
 
-	-- Сброс
-	i_rst <= '1';
-	wait_clk(2);
-	
-	i_result_src <= "11";
-
-	-- Завершение симуляции
-	wait;
+  -- Завершение симуляции
+  wait;
 end process;
 
 
